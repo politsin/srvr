1066bf28688fc3ee9f96af54b1bdbef2aaed7910
Anatoly Politsin <politsin@gmail.com>
2023-11-15 13:08:36 +0000

    ssl

--- accets/docker-proxy/start.sh
+++ accets/docker-proxy/start.sh
@@ -4,4 +4,17 @@ cd /opt/docker-proxy
 if [ ! -f /opt/docker-proxy/default/ssl/dhparam-2048.pem ]; then
   openssl dhparam -out /opt/docker-proxy/default/ssl/dhparam-2048.pem 2048
 fi
+
+KEYS=/opt/docker-proxy/default/ssl/proxy.crt
+if [ -f "$KEYS" ]; then
+    echo "$KEYS exists."
+else
+  openssl req -x509 -nodes -days 9000 -newkey rsa:2048 \
+    -keyout /opt/docker-proxy/default/ssl/proxy.key \
+    -out /opt/docker-proxy/default/ssl/proxy.crt
+  sed -i -e 's/server.crt/proxy.crt/g' /opt/docker-proxy/default/default.conf
+  sed -i -e 's/server.key/proxy.key/g' /opt/docker-proxy/default/default.conf
+  echo "$KEYS done."
+fi
+
 docker-compose up -d docker-proxy
