81e1d776ba4e603ff31d6b2ca204b8eb9b4ee3a1
Anatoly Politsin <politsin@gmail.com>
2023-11-24 20:48:26 +0000

    refa

--- accets/apps/certbot/recurrent.sh
+++ accets/apps/certbot/recurrent.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 HOST_NAME="${HOST}"
-mkdir -p /opt/apps/certbot/tls
+mkdir -p ./tls
 if [[ ! -z "$1" ]]; then
   HOST_NAME=$1
-  rm /opt/apps/certbot/tls/*
-  ln -s /opt/apps/certbot/etc/live/$HOST_NAME/privkey.pem /opt/apps/certbot/tls/private.pem
-  ln -s /opt/apps/certbot/etc/live/$HOST_NAME/fullchain.pem /opt/apps/certbot/tls/fullchain.pem
+fi
+  rm -f ./tls/*
+  ln -s ../etc/live/$HOST_NAME/privkey.pem ./tls/private.pem
+  ln -s ../etc/live/$HOST_NAME/fullchain.pem ./tls/fullchain.pem
   chown -R 999:999 ./tls/private.pem
   chown -R 999:999 ./tls/fullchain.pem
-fi
 echo "$HOST_NAME"
