ad984bd81c1ed32e9334ae18a37c9231354b9569
Anatoly Politsin <politsin@gmail.com>
2023-12-17 14:15:45 +0000

    crt-subj

--- accets/docker-rest/genKeys.sh
+++ accets/docker-rest/genKeys.sh
@@ -4,7 +4,10 @@ KEYS=/opt/docker-rest/ssl/rest.crt
 if [ -f "$KEYS" ]; then
     echo "$KEYS exists."
 else
-  openssl req -x509 -nodes -days 9000 -newkey rsa:2048 -keyout /opt/docker-rest/ssl/rest.key -out /opt/docker-rest/ssl/rest.crt
+  openssl req -x509 -nodes -days 9000 -newkey rsa:2048 \
+    -keyout /opt/docker-rest/ssl/rest.key \
+    -out /opt/docker-rest/ssl/rest.crt \
+    -subj "/C=RU/ST=Msc/L=Msc/O=Syn/OU=IT/CN=example.com"
   sed -i -e 's/example.crt/rest.crt/g' /opt/docker-rest/nginx.conf
   sed -i -e 's/example.key/rest.key/g' /opt/docker-rest/nginx.conf
   echo "$KEYS done."
