b4350d8f7f4cd6a4b6c2c88f97cce2317a075a35
Anatoly Politsin <politsin@gmail.com>
2024-02-12 20:27:07 +0000

    fix

--- src/System/ArchArm.php
+++ src/System/ArchArm.php
@@ -5,7 +5,7 @@ namespace Srvr\System;
 /**
  * System architecture Arm/aarch64.
  */
-abstract class ArchArm extends Linux {
+class ArchArm extends Linux {
 
   //phpcs:disable
   protected string $arch = 'aarch64';
