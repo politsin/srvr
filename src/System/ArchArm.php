308a9707158a645d975a297e256aaeb1489e0694
Anatoly Politsin <politsin@gmail.com>
2024-02-12 20:31:01 +0000

    fix

--- src/System/ArchArm.php
+++ src/System/ArchArm.php
@@ -12,8 +12,8 @@ class ArchArm extends Linux {
   protected array $extras =[
     // 'apt install lm-sensors i2c-tools',
     'apt install php8.1-gmp -y --no-install-recommends',
-    'pecl install -y dio',
-    'pecl install -y channel://pecl.php.net/dio-0.2.1',
+    'pecl install dio',
+    'pecl install channel://pecl.php.net/dio-0.2.1',
     'echo "extension=dio.so" > /etc/php/8.1/mods-available/dio.ini',
     'ln -s /etc/php/8.1/mods-available/dio.ini /etc/php/8.1/cli/conf.d/20-dio.ini',
   ];
