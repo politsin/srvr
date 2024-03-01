7ad4a75c721a5b05989f582459cdb9792ae25f2f
Anatoly Politsin <politsin@gmail.com>
2024-03-01 13:42:58 +0000

    me

--- src/Step/Step8GitUser.php
+++ src/Step/Step8GitUser.php
@@ -14,7 +14,8 @@ class Step8GitUser extends Step0Base {
     $this->exec(['git', 'config', '--global', 'user.name', 'Anatoly' . 'Politsin']);
     $this->exec(['git', 'config', '--global', 'user.email', 'politsin@gmail.com']);
     $this->exec(['git', 'config', '--global', 'push.default', 'simple']);
-    $result = $this->exec(['/usr/bin/cat', '~/.gitconfig']);
+    $me = $this->exec(['whoami']);
+    $result = $this->exec(['/usr/bin/cat', "/$me/.gitconfig"]);
     dump($result);
     return 1;
   }
