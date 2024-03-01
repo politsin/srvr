61994c6f16d25d4e89fe2c82d6139966f6e705f5
Anatoly Politsin <politsin@gmail.com>
2024-03-01 13:39:52 +0000

    ok

---
+++ src/Step/Step8GitUser.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Srvr\Step;
+
+/**
+ * Step8 Clear.
+ */
+class Step8GitUser extends Step0Base {
+
+  /**
+   * Run!
+   */
+  public function run() : bool {
+    $this->exec(['git', 'config', '--global', 'user.name', 'Anatoly' . 'Politsin']);
+    $this->exec(['git', 'config', '--global', 'user.email', 'politsin@gmail.com']);
+    $this->exec(['git', 'config', '--global', 'push.default', 'simple']);
+    $result = $this->exec(['cat', '~/.gitconfig']);
+    dump($result);
+    return 1;
+  }
+
+}
