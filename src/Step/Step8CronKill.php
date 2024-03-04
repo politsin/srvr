cb18f9a144dff0b20e8df9f7a68bc53c6ed9abf8
Anatoly Politsin <politsin@gmail.com>
2024-03-04 13:47:00 +0000

    php

--- src/Step/Step8CronKill.php
+++ src/Step/Step8CronKill.php
@@ -16,8 +16,8 @@ class Step8CronKill extends Step0Base {
       "* * * * * ps -aux | grep phpcs | awk '{print $2}' | xargs kill",
       "* * * * * ps -aux | grep phpcbf | awk '{print $2}' | xargs kill",
       "30 7,12,18 * * * ps -aux | grep vscode | awk '{print $2}' | xargs kill",
-      '*/10 * * * *  echo "" > $(docker inspect --format='{{.LogPath}}' docker-proxy)',
-      '*/30 * * * *  echo "" > /opt/sites/2130-gruzovozkin/www-home/log/nginx-access.log',
+      // '*/10 * * * *  echo "" > $(docker inspect --format='{{. LogPath}}' docker-proxy)',
+      // '*/30 * * * *  echo "" > /opt/sites/XX-grxxxin/www-home/log/nginx-access.log',
     ];
     $this->io->warning("@todo set cron");
     $this->io->error(implode("\n", $cron));
