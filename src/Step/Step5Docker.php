624f2271e04643691194ba7a2b86b9597e533d07
Anatoly Politsin <politsin@gmail.com>
2024-04-19 11:14:43 +0000

    upd

--- src/Step/Step5Docker.php
+++ src/Step/Step5Docker.php
@@ -15,10 +15,13 @@ class Step5Docker extends Step0Base {
     $arch = "x86_64";
     $this->io->warning("TODO: arch: $arch");
     $compose = "https://github.com/docker/compose/releases/download/{$version}/docker-compose-linux-$arch";
+    $dir = "/usr/local/lib/docker/cli-plugins";
     $this->execCommands([
       'apt install -y docker.io docker-compose',
-      "curl -SL $compose -o /usr/local/lib/docker/cli-plugins/docker-compose",
-      "chmod +x /usr/local/lib/docker/cli-plugins/docker-compose",
+      "mkdir -p $dir",
+      "rm -f $dir/docker-compose",
+      "curl -SL $compose -o $dir/docker-compose",
+      "chmod +x $dir/docker-compose",
     ]);
     return 1;
   }
