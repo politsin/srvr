632dcc4f4d04edceec3e2ce9b89e07260558d9c3
Anatoly Politsin <politsin@gmail.com>
2024-08-16 06:47:13 +0000

    swap

--- src/Step/Step4Swap.php
+++ src/Step/Step4Swap.php
@@ -15,19 +15,37 @@ class Step4Swap extends Step0Base {
       'swapon --show=SIZE',
       'free --giga -h -t',
     ];
-    $swapon = [
-      'fallocate -l 4G /swapfile',
-      'chmod 600 /swapfile',
-      'mkswap /swapfile',
-      'swapon /swapfile',
-      'echo "/swapfile none swap sw 0 0" >> /etc/fstab',
-    ];
-    if (!file_exists('/swapfile')) {
-      $this->execCommands($swapon);
+    $current = $this->currentMem();
+    if ($current['swap'] < $current['mem']) {
+      $swap = $current['mem'] - $current['swap'] = 2;
+      $this->io->warning("Swap ON {$swap}G");
+      $swapon = [
+        "fallocate -l {$swap}G /swapfile",
+        'chmod 600 /swapfile',
+        'mkswap /swapfile',
+        'swapon /swapfile',
+        'echo "/swapfile none swap sw 0 0" >> /etc/fstab',
+      ];
+      if (!file_exists('/swapfile')) {
+        $this->execCommands($swapon);
+      }
     }
+
     $result = $this->execCommands($cmd);
     dump($result);
     return 1;
+  }
+
+  /**
+   * Current Mem.
+   */
+  private function currentMem() : array {
+    $current = $this->exec('free -g');
+    $lines = explode("\n", $current);
+    return [
+      'mem' => trim(substr($lines[1], 15, 6)),
+      'swap' => trim(substr($lines[2], 15, 6)),
+    ];
   }
 
 }
