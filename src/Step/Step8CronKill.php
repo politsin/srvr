bcad82eb465132a804abea1234d479d47651091b
Anatoly Politsin <politsin@gmail.com>
2024-09-16 09:44:55 +0000

    telegraaf

--- src/Step/Step8CronKill.php
+++ src/Step/Step8CronKill.php
@@ -35,4 +35,22 @@ class Step8CronKill extends Step0Base {
     return 1;
   }
 
+  /**
+   *
+   */
+  private function template() {
+    $tpl = <<<EOD
+# Dev: phpcs phpcbf vscode
+* * * * * ps -aux | grep phpcs | awk '{print $2}' | xargs kill
+* * * * * ps -aux | grep phpcbf | awk '{print $2}' | xargs kill
+30 7,12,18 * * * ps -aux | grep vscode | awk '{print $2}' | xargs kill
+
+# Apps
+0 10 * * * /usr/bin/docker start certbot -a > /opt/apps/certbot/log/cron.log
+30 9 * * * /usr/bin/docker exec -i docker-proxy /usr/sbin/nginx -s reload > /opt/docker-proxy/reload-log.log
+
+# Custom
+    EOD;
+  }
+
 }
