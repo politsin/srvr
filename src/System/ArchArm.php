2bfb23db99ff117735cbbdfc2ad9cc3a8984dc89
root <root@orangepizero2>
2024-12-16 10:26:44 +0000

    arm

--- src/System/ArchArm.php
+++ src/System/ArchArm.php
@@ -9,15 +9,7 @@ class ArchArm extends Linux {
 
   //phpcs:disable
   protected string $arch = 'aarch64';
-  protected array $extras =[
-    // 'apt install lm-sensors i2c-tools',
-    'apt install php-mbstring php8.1-gmp -y --no-install-recommends',
-    // 'pecl install dio',
-    'pecl install channel://pecl.php.net/dio-0.2.0',
-    // 'pecl install channel://pecl.php.net/dio-0.3.0RC1',
-    'echo "extension=dio.so" > /etc/php/8.1/mods-available/dio.ini',
-    'ln -s /etc/php/8.1/mods-available/dio.ini /etc/php/8.1/cli/conf.d/20-dio.ini',
-  ];
+  protected string $dio = "0.3.0";
   //phpcs:enable
   // # PhpCS:::
   //  mkdir /var/lib/composer && \
@@ -31,11 +23,20 @@ class ArchArm extends Linux {
   //  /var/lib/composer/vendor/bin/phpcs --config-show
   // custom bin
   // .bashrc: export PATH="$PATH:/opt/console/vendor/bin"
-
+  
   /**
    * Commands.
    */
   public function getCommands() : array {
+    $php ="{$version[0]}.{$version[1]}";
+    $extras =[
+      // 'apt install lm-sensors i2c-tools',
+      "apt install php-mbstring php{$php}-gmp -y --no-install-recommends",
+      // 'pecl install dio',
+      "pecl install channel://pecl.php.net/dio-{$dio}",
+      "echo 'extension=dio.so' > /etc/php/{$php}/mods-available/dio.ini",
+      "ln -s /etc/php/{$php}/mods-available/dio.ini /etc/php/{$php}/cli/conf.d/20-dio.ini",
+  ]  ;
     return $this->extras;
   }
 
